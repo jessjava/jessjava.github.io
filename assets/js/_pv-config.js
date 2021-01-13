66e6b526d012ce945aa47939669dbd5752bf52f0
wujia8 <wujia8@jd.com>
2021-01-13 21:08:42 +0800

    remove-google-ananlys

--- assets/js/_pv-config.js
+++ assets/js/_pv-config.js
@@ -5,10 +5,4 @@
 const proxyEndpoint = "{{ site.google_analytics.pv.proxy_endpoint }}";
 
 
-{% if site.google_analytics.pv.cache and site.google_analytics.pv.enabled %}
-  {% assign enabled = true %}
-{% else %}
-  {% assign enabled = false %}
-{% endif %}
-
 const pvCacheEnabled = {{ enabled }};
